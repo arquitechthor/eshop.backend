b55e11d07106772cc0aa0400135e71fdc2ef3520
arquitechthor <arquitechthor@gmail.com>
2023-11-23 19:29:03 -0400

    Feat: Unit Test and Services
    Added Service.
    Added Unit test for Service.
    Database schema added.
    Configuration fixed for load data from schema.
    Repository files added.
    Principal Query for challenge added.
    Added Request and Response objects.

--- src/main/java/com/arquitechthor/eshop/entity/Brand.java
+++ src/main/java/com/arquitechthor/eshop/entity/Brand.java
@@ -4,11 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @Entity
+@AllArgsConstructor
 @Table(name = "BRANDS")
 public class Brand {
 
--- src/main/java/com/arquitechthor/eshop/entity/Product.java
+++ src/main/java/com/arquitechthor/eshop/entity/Product.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @Entity
+@AllArgsConstructor
 @Table(name = "PRODUCTS")
 public class Product {
 
